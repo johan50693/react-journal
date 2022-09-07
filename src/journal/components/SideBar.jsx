cbe124dd86af7343f01f75aaf21d9d9d6771d074
Albert Urbina <johan50693@gmail.com>
2022-09-07 19:36:29 -0400

    agregar notas a sidebar

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -2,10 +2,12 @@ import { TurnedInNot } from "@mui/icons-material"
 import { Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from "@mui/material"
 import { Box } from "@mui/system"
 import { useSelector } from "react-redux"
+import { SideBarItem } from "./SideBarItem"
 
 export const SideBar = ({drawerWidth=240}) => {
 
   const { displayName} = useSelector( state => state.auth);
+  const { notes } = useSelector( state => state.journal);
 
   return (
     <Box
@@ -33,18 +35,8 @@ export const SideBar = ({drawerWidth=240}) => {
         
         <List>
           {
-            ['Enero','Febrero','Marzo','Abril'].map( text => (
-              <ListItem key={text} disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    <TurnedInNot/>
-                  </ListItemIcon>
-                  <Grid container>
-                    <ListItemText primary={text} />
-                    <ListItemText secondary={'Este es el contenido del texto principal'} />
-                  </Grid>
-                </ListItemButton>
-              </ListItem>
+            notes.map( note => (
+              <SideBarItem key={note.id} {...note} />
             ))
           }
         </List>
